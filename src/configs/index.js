8ebd7c1def5998301cdc97dd84c1be9320c6823a
KnifeZ <knifezred@outlook.com>
2020-11-24 22:43:28 +0800

    add some js

--- src/configs/index.js
+++ src/configs/index.js
@@ -3,4 +3,20 @@ export default {
      * @description 配置显示在浏览器标签的title
      */
     title: 'KnifeZ.Virgo with Vue',
+    /**
+     * @description 版本号
+     */
+    version: process.env.VUE_APP_VERSION,
+    /**
+     * debugger调试
+     */
+    development: process.env.NODE_ENV !== "production",
+    /**
+     *  cookies 过期时间
+     */
+    cookiesExpires: 365,
+    /**
+     * iframe 嵌入页面标示
+     */
+    staticPage: "@StaticPage",
 }
